5ab73f33946f3a72cf4613dfeb48391c10d2cccf
Beila H <beilah@nycstudents.net>
2025-04-04 09:27:16 -0400

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -14,11 +14,29 @@ class Flashcard:
         return {"make": self.make, "dex": self.dex, "type": self.type, "image": self.image}
     
 pokemon = [
-    Flashcard("Reshiram", "644", "Dragon/Fire", "reshiram_image.jpg"),
-    Flashcard("Zekrom", "644", "Dragon/Electric", "zekrom_image.jpg"),
-    Flashcard("Charizard", "006", "Fire/Flying", "charizard_image.jpg"),
-    Flashcard("Venusaur", "003", "Grass/Poison", "venusaur_image.jpg")
+    Flashcard("Reshiram", "643", "Dragon/Fire", "reshiram.jpg"),
+    Flashcard("Zekrom", "644", "Dragon/Electric", "zekrom.jpg"),
 ]
 
+# Convert objects to dictionaries
+pokemon_data = [Flashcard.to_dict() for Flashcard in pokemon]
 
+# Save to a JSON file
+with open("FlashCards.json", "w") as file:
+    json.dump(pokemon_data, file, indent = 4)
 
+new_pokemon = Flashcard("Chevrolet", "Malibu", 2024, "malibu_image.jpg")
+
+# Load existing data
+try:
+    with open("FlashCards.json", "r") as file:
+        pokemon_data = json.load(file)
+except FileNotFoundError:
+    pokemon_data = []
+
+# Append new poke
+pokemon_data.append(new_pokemon.to_dict())
+
+# Save updated data back to file
+with open("cars.json", "w") as file:
+    json.dump(pokemon_data, file, indent=4)
